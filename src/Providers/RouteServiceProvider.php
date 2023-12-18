02d5d290c7106f3af55373ee8748119026d74bad
Bushra Asif <bushra@web-vision.de>
2023-12-18 14:30:42 +0500

    Add script to publish migration

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -3,11 +3,17 @@
 namespace Webvision\NinjaZugferd\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
 
 class RouteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
+        Log::warning(__DIR__);
+
+        $this->publishes([
+            __DIR__.'/database/migrations/' => database_path('migrations')
+        ], 'migrations');
     }
 }
