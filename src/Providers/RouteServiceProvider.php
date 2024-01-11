134f10423c3f0726a205b68b3724909075dc5420
Bushra Asif <bushra@web-vision.de>
2024-01-11 10:36:23 +0500

    Remove migration script

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -11,11 +11,5 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
-        // Log::warning("Testing");
-        // $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->commands([
-            MigrateNinjaxRechnung::class,
-        ]);
-        // Log::warning(__DIR__ . '/database/migrations');
     }
 }
